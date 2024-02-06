68781c0a831cfddecffbe88998a57f1f91de4b74
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2024-02-06 17:20:30 +0200

    update

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Activate the virtual environment
-source /somedirectoryscript/gui_white_rabbit/venv/bin/activate
+source /somedirectoryname/gui_white_rabbit/venv/bin/activate
 
 # Change directory to where your Python script is located
-cd /somedirectoryscript/github/gui_white_rabbit
+cd /somedirectoryname/github/gui_white_rabbit
 
 # Run the Python script
 python3 bot.py > /dev/null 2>&1 &
